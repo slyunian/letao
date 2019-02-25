99f4dcaa562578b2cefa5efdba8c0c5999d5700a
slyunian <1181717028@qq.com>
2019-02-25 09:33:22 +0800

    二级分类功能基本完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -13,7 +13,7 @@ function render() {
         },
         dataType: 'json',
         success: function (info) {
-            console.log(info);
+            // console.log(info);
             var htmlStr = template('secondTb', info);
             $('tbody').html(htmlStr);
             // 渲染分页
@@ -43,7 +43,7 @@ $('#addItem').on('click', function () {
         },
         dataType: 'json',
         success: function (info) {
-            console.log(info);
+            // console.log(info);
 
             var htmlStr = template('downBar', info);
             $('.dropdown-menu').html(htmlStr);
@@ -52,7 +52,7 @@ $('#addItem').on('click', function () {
 })
 
 // 点击ul选中
-$('.dropdown-menu').on('click', 'li', function () {
+$('.dropdown-menu').on('click', 'li a', function () {
     $('#dropdownText').text($(this).text());
     $('[name="categoryId"]').val($(this).data("id"));
     $('#form').data("bootstrapValidator").updateStatus("categoryId", "VALID");
@@ -60,22 +60,17 @@ $('.dropdown-menu').on('click', 'li', function () {
 
 // 配置图片上传
 $('#fileupload').fileupload({
-    // 指定数据类型为 json
-    dataType: "json",
-    // done, 当图片上传完成, 响应回来时调用
+    dataType: 'json',
+    // e事件对象
+    // ?data:图片伤处后的对象通过data.result.picAddr可以获取图片上传后的图片地址
     done: function (e, data) {
-        console.log(data)
-        // 获取上传成功的图片地址
-        var picAddr = data.result.picAddr;
-        // 设置图片地址
-        $('#imgBox img').attr("src", picAddr);
-        // 将图片地址存在隐藏域中
-        $('[name="brandLogo"]').val(picAddr);
-
-        // 重置校验状态
-        $('#form').data("bootstrapValidator").updateStatus("brandLogo", "VALID")
+        console.log(data.result.picAddr);
+        var picURL = data.result.picAddr;
+        $('#showImg').attr('src', picURL);
+        $('[name="brandLogo"]').val(picURL);
+        $('#form').data('bootstrapValidator').updateStatus('brandLogo', 'VALID')
     }
-});
+})
 
 $('#form').bootstrapValidator({
 
@@ -118,3 +113,30 @@ $('#form').bootstrapValidator({
         }
     }
 });
+
+
+// 点击添加发送ajax请求
+$('#addBtn').on('click', function (e) {
+    // 阻止默认事件
+    e.preventDefault();
+    // 发送ajax请求
+    $.ajax({
+        type: 'post',
+        url: '/category/addSecondCategory',
+        data: $('#form').serialize(),
+        dataType: 'json',
+        success: function (info) {
+            if (info.success) {
+                $('#addModal').modal('hide');
+                $('#form').data('bootstrapValidator').resetForm(true);
+                currentPage = 1;
+                render();
+                // 找到下拉菜单文本重置
+                $('#dropdownText').text("请选择1级分类")
+
+                // 找到图片重置
+                $('#imgBox img').attr("src", "images/none.png")
+            }
+        }
+    })
+})
